94407c3f4518032b3edaf625c48e7f94e4e70eed
Márton Kardos <power.up1163@gmail.com>
2022-12-14 11:14:09 +0100

    mouse tracking data aggregation

--- src/mess_around.py
+++ src/mess_around.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 
 from utils.fetch import fetch_experiment_data
-from utils.mouse import auc
+from utils.mouse import aggregate_mouse_tracking
 
 # %%
 dat = fetch_experiment_data()
@@ -18,26 +18,5 @@ dat = fetch_experiment_data()
 # %%
 dat
 # %%
-mouse = dat.mouseTrackingData.loc[3]
-
-# %%
-auc(mouse)
-
-# %%
-dat["auc"] = dat.mouseTrackingData.map(auc) / (
-    dat.screenWidth * dat.screenHeight
-)
-dat
-
-# %%
-mouse_df = pd.DataFrame.from_records(mouse)
-# %%
-mouse_df = pd.DataFrame.from_records(mouse)
-mouse_df["dx"] = mouse_df["x"] - mouse_df["x"].shift()
-mouse_df["dy"] = mouse_df["y"] - mouse_df["y"].shift()
-mouse_df["dt"] = mouse_df["timestamp"] - mouse_df["timestamp"].shift()
-mouse_df["distance"] = np.sqrt(mouse_df["dx"] ** 2 + mouse_df["dy"] ** 2)
-mouse_df["velocity"] = mouse_df["distance"] / mouse_df["dt"]
-mouse_df
-
+aggregate_mouse_tracking(dat)
 # %%
